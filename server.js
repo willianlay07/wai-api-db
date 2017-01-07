436bf0fa080c70864c4005bfcb38ba51908a8e8b
willianlay07 <willianlay07@gmail.com>
2017-01-07 23:47:54 +0800

    Update todos

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var _ 				= require('underscore');
 
 var app 			= express();
 var PORT 			= process.env.PORT || 3000;
-var todos 			= [];
+var todos 			= [{
+		id: 1,
+		description: 'Walk a dog',
+		complete: true
+	},
+	{
+		id: 2,
+		description: 'Clean a room',
+		complete: false
+	}
+];
 var todoNextId 		= 1;
 
 app.use(bodyParser.json());		// middleware;
@@ -12,6 +22,12 @@ app.use(bodyParser.json());		// middleware;
 app.get('/', function(req, res){
 	res.send('Hello World! Welcome to API - DB');
 });
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
