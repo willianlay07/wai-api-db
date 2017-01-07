4e93cd84887964c847b4e2b6ce564910d2d36179
willianlay07 <willianlay07@gmail.com>
2017-01-07 23:26:14 +0800

    Add Express Module

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Wai');
+var express 		= require('express');
+var bodyParser 		= require('body-parser');
+var _ 				= require('underscore');
+
+var app 			= express();
+var PORT 			= process.env.PORT || 3000;
+var todos 			= [];
+var todoNextId 		= 1;
+
+app.use(bodyParser.json());		// middleware;
+
+app.get('/', function(req, res){
+	res.send('Hello World! Welcome to API - DB');
+});
+
+app.listen(PORT, function(){
+	console.log('Express listening on port ' + PORT + '!');
+});
